77014ae2ee44b201416dc73ae0a603b1581d0cfe
Raparla Karthikeya <karthikeyar1811@gmail.com>
2026-01-19 16:31:11 +0530

    Array is added

---
+++ Array/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int[] age = { 24, 23, 18, 19, 20 };
+            
+        }
+    }
+}
+
+

e1c8de138dc31d9755b2158b92a943d8d4516b5c
Karthikeya Raparla <113329344+karthikeyaraparla@users.noreply.github.com>
2026-01-29 18:06:20 +0530

    Add files via upload

---
+++ Review 2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+class Parent
+{
+    protected List<int> numbers;
+
+    public Parent(List<int> numbers)
+    {
+        this.numbers = numbers;
+    }
+    public virtual void Process()
+    {
+        Console.WriteLine("Processing");
+    }
+}
+class NumProcessor : Parent
+{
+    public NumProcessor(List<int> numbers) : base(numbers)
+    {
+    }
+
+    public override void Process()
+    {
+        var filtered = numbers.Where(n => n >= 0).ToList();
+        var squared = filtered.Select(n => n * n).ToList();
+        squared.Sort();
+
+        foreach (int num in squared)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        List<int> inputNums = new List<int> { -4, -1, 0, 3, 10 };
+        NumProcessor processor = new NumProcessor(inputNums);
+        processor.Process();
+    }
+}
